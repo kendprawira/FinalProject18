a49d0507e33837172cddc70ff43372f8138c89a5
kendprawira <kendprawira@gmail.com>
2025-10-02 15:27:42 +0700

    bebas

--- tets18_12.java
+++ tets18_12.java
@@ -1,6 +1,6 @@
 public class tets18_12 {
     public static void main(String[] args) {
-        System.out.println("Halo, ini baris kode pertama yang dimodifikasi Ferdy12");
-        System.out.println("Ferdy12 menambahkan baris baru");
+        System.out.println("Halo, ini baris kode pertama yang dimodifikasi ken18");
+        System.out.println("ken18 menambahkan baris baru");
     }
 }
