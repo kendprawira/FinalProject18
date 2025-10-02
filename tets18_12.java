f6ae861ddb86eb134f28e53d22ed213d8f52691c
ferthereaper <ferdyansahprakoso55@gmail.com>
2025-10-02 15:10:08 +0700

    testken

--- tets18_12.java
+++ tets18_12.java
@@ -1,6 +1,6 @@
 public class tets18_12 {
     public static void main(String[] args) {
-        System.out.println("Halo, ini baris kode pertama yang dimodifikasi ken");
-        System.out.println("ken menambahkan baris baru");
+        System.out.println("Halo, ini baris kode pertama yang dimodifikasi Ferdy");
+        System.out.println("Ferdy menambahkan baris baru");
     }
 }
