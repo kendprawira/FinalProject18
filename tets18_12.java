58a54ec6890373ff6e10ffbf870e83cfbd4862e7
ferthereaper <ferdyansahprakoso55@gmail.com>
2025-10-02 15:30:42 +0700

    beabs

--- tets18_12.java
+++ tets18_12.java
@@ -1,6 +1,6 @@
 public class tets18_12 {
     public static void main(String[] args) {
-        System.out.println("Halo, ini baris kode pertama yang dimodifikasi Ferdy12");
-        System.out.println("Ferdy12 menambahkan baris baru");
+        System.out.println("Halo, ini baris kode pertama yang dimodifikasi Ferdy_12");
+        System.out.println("Ferdy_12 menambahkan baris baru");
     }
 }
